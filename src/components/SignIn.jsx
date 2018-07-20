aa8a48a1416accb61a79b09b5af91734924f584d
Vick Kumar <vick.kumar44@gmail.com>
2018-07-19 20:59:36 -0400

    got the signup form working

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -4,7 +4,7 @@ class SignIn extends Component{
   render(){
     return(
       <div>
-        <h1>Sign In</h1>
+        
       </div>
     )
   }
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,10 +1,43 @@
 import React, { Component } from 'react'
-
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import {firebaseApp} from '../firebase'
 class SignUp extends Component{
+  state = {
+    email: '',
+    password: '',
+    error: {
+      message: ''
+    }
+  }
+  signUp(){
+    console.log('this.state', this.state)
+    const {email, password} = this.state
+    firebaseApp.auth().createUserWithEmailAndPassword(email, password)
+      .catch(err => this.setState({error: err}))
+  }
   render(){
     return(
       <div>
         <h1>SignUp</h1>
+        <form  noValidate style={{display: 'flex', flexDirection: 'column', margin: '10px'}}>
+          <TextField
+            id="email"
+            label="Email"
+            margin="normal"
+            onChange={e => this.setState({email: e.target.value})}
+          />
+          <TextField
+            id="password"
+            label="Password"
+            type="password"
+            margin="normal"
+            onChange={e => this.setState({password: e.target.value})}
+          />
+          <Button variant="contained" color="primary" onClick={() => this.signUp()}>Sign Up</Button>
+          <p>{this.state.error.message}</p>
+        </form>
       </div>
     )
   }
