6ffeb30283906b8d0e07d35b367bdc9dc5e754bb
Vick Kumar <vick.kumar44@gmail.com>
2018-07-19 20:03:37 -0400

    setup routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+// Components
+import App from './components/App'
+import SignUp from './components/SignUp'
+import SignIn from './components/SignIn'
 
 ReactDOM.render(
-  <div>Hello</div>, document.querySelector('root')
+  <Router>
+    <div>
+      <Route exact path='/' component={App} />
+      <Route path='/signin' component={SignIn} />
+      <Route path='/signup' component={SignUp} />
+    </div>
+  </Router>, 
+  document.getElementById('root')
 )
