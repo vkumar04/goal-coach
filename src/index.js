882847807a294c79fbc95b716c8904f6a5da33c3
Vick Kumar <vick.kumar44@gmail.com>
2018-07-19 20:17:55 -0400

    added material fonts

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
-
+import {firebaseApp} from './firebase'
 // Components
 import App from './components/App'
 import SignUp from './components/SignUp'
 import SignIn from './components/SignIn'
+
+firebaseApp.auth().onAuthStateChanged(user => {
+  user ? console.log('user signed in', user) :
+  console.log('user does not exist or logged out')
+})
 
 ReactDOM.render(
   <Router>
